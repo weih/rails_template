bacac0dde572291e8042687f2f9c5d1754047378
Hao Wei <imwillmouse@gmail.com>
2014-04-14 08:08:34 +0800

    Updated

--- rails_template.rb
+++ rails_template.rb
@@ -1,19 +1,3 @@
 run("sed -i '' '1d' Gemfile")
 
 add_source 'http://ruby.taobao.org'
-
-gem 'bourbon'
-gem 'neat', '~> 1.5.0'
-
-gem_group :development do
-  gem 'thin'
-  gem 'better_errors'
-  gem 'binding_of_caller'
-  gem 'rails-footnotes', '>= 4.0.0'
-end
-
-run('bower install backbone')
-
-# generate('rails_footnotes:install')
-# 
-# run("bunlde exec rails generate rails_footnotes:install")
