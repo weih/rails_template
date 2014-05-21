f71ed461b0390539f7fecc13df7ef99b8c289e39
Hao Wei <imwillmouse@gmail.com>
2014-05-22 03:44:03 +0800

    updated

--- rails_template.rb
+++ rails_template.rb
@@ -1,3 +1,37 @@
 run("sed -i '' '1d' Gemfile")
 
 add_source 'http://ruby.taobao.org'
+
+if yes?("Want Thoughtbot Bourbon + Neat?")
+  gem 'bourbon'
+  gem 'neat', '~> 1.5.0'
+end
+
+gem_group :development do
+  gem 'thin'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'rails-footnotes', '>= 4.0.0'
+  gem 'pry-rails'
+  gem 'pry-debugger'
+  gem 'pry-rescue'
+  gem 'pry-stack_explorer'
+  gem "bower-rails", "~> 0.7.1"
+  gem 'quiet_assets'
+  gem 'marginalia'
+end
+
+gem_gruop :test do
+  gem 'rspec'
+end
+
+rakefile("boot.rake") do
+  <<-TASK
+task :boot do
+  system("rails g bower_rails:initialize")
+  system("rails generate rails_footnotes:install")
+end
+  TASK
+end
+
+# run('bower install backbone')
